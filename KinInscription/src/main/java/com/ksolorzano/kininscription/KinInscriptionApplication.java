d36ad0343c83507e514727782141a6c89488f6db
ksolo-2022439 <ksolorzano-2022439@kinal.edu.gt>
2025-10-06 15:41:23 -0600

    Initial Commit

---
+++ KinInscription/src/main/java/com/ksolorzano/kininscription/KinInscriptionApplication.java
@@ -0,0 +1,13 @@
+package com.ksolorzano.kininscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinInscriptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinInscriptionApplication.class, args);
+    }
+
+}
---
+++ KinInscription/src/test/java/com/ksolorzano/kininscription/KinInscriptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksolorzano.kininscription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinInscriptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
